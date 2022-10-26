f548794f09aa3c21cc84602e31181f465abff1fe
Adrian Pistol <vifino@tty.sh>
2022-10-26 04:52:40 +0200

    pkgs/vpp: Make it compile. (#3)
    * pkgs/vpp: Make it compile.
    
    Still needs some work.
    
    * pkgs/vpp: Fix plugin path and DPDK.

--- pkgs/vpp/default.nix
+++ pkgs/vpp/default.nix
@@ -1,21 +1,64 @@
-{ stdenv, pkgs }:
+{ stdenv, pkgs, lib,
+runtimeShell, python3,
+enableDpdk ? true,
+enableAfXdp ? false}:
 
 stdenv.mkDerivation rec {
   name = "vpp-${version}";
-  version = "git-2022-07-21";
+  version = "22.10-rc2";
   src = pkgs.fetchFromGitHub {
     owner = "FDio";
     repo = "vpp";
-    rev = "c8cd079a0004b75892a08c7cac9a48b39e24e580";
-    hash = "sha256-fx6J/g8BQYf93VGJLR+Hx/wu16Hg+ijkrRP89NnP9e0=";
+    rev = "v${version}"; #"61bae8a54d14899337b0d0a7ca9b9367f6321951";
+    hash = "sha256-3b+cnEjbReCg+svVD4DZwgcLwoA/IDWTGoP9ALYZFR4=";
   };
+  sourceRoot = "source/src";
 
-  nativeBuildInputs = with pkgs; [ pkg-config curl git cmake ninja nasm ];
+  nativeBuildInputs = with pkgs; [ pkg-config curl git cmake ninja nasm coreutils ];
   buildInputs = with pkgs; [
     libconfuse numactl libuuid
-    libffi libnl openssl
-    dpdk
-  ];
-  buildPhase = "make build-release";
-  installPhase = "install -Dm755 example $out";
+    libffi openssl
+
+    python3.pkgs.wrapPython(python3.withPackages (pp: with pp; [
+      ply # for vppapigen
+    ]))
+
+    # linux-cp deps
+    libnl libmnl
+
+    #libbpf
+    #libelf
+  ]
+  # dpdk plugin
+  ++ lib.optional enableDpdk [ dpdk libpcap jansson ]
+  # af_xdp deps - broken: af_xdp plugins - no working libbpf found - af_xdp plugin disabled
+  ++ lib.optional enableAfXdp libbpf
+
+  ++ lib.optional (enableDpdk || enableAfXdp) libelf;
+
+  # Needs patches..
+  patchPhase = ''
+    # This attempts to use git to fetch the version, but we already know it.
+    printf "#!${runtimeShell}\necho '${version}'\n" > scripts/version
+    chmod +x scripts/version
+    ./scripts/version
+    substituteInPlace pkg/CMakeLists.txt --replace 'file(READ "/etc/os-release" os_release)' 'set(os_release "NAME=NIX; ID=nix")'
+
+    patchShebangs .
+  '';
+
+  enableParallelBuilding = true;
+  cmakeFlags = [ 
+    "-DCMAKE_INSTALL_LIBDIR=lib" # wants a relative path
+    
+    # For debugging CMake:
+    #"--trace-source=CMakeLists.txt"
+    #"--trace-expand"
+  ]
+  # Link against system DPDK. Note that this is actually statically linked as well.
+  ++ lib.optional enableDpdk "-DVPP_USE_SYSTEM_DPDK=true";
+
+  # TODO: Add service
+  # TODO: Add users/group for default config.
+  # TODO: RDMA plugin - needs libibverbs.a, can probably get that from rdma-core if not removing static libs?
 }
